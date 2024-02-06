989738a1cae414dc7f63531f1074f36004fc28b5
Monesha <moneshadhali@gmail.com>
2024-02-06 16:57:49 +0000

    Computer class created

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,3 +1,29 @@
 public class Computer {
-    
+
+    private int storage;
+    private String model;
+
+    public Computer(int storage){
+        this.storage = storage;
+    }
+
+    public int getStorage(){
+        return storage;
+    }
+
+    public void setModel(String newModel){
+        this.model = newModel;
+    }
+
+    public void addStorage(int extraStorage ){
+        System.out.println("How much storage do you want to add??");
+        this.storage += extraStorage;
+    }
+
+    public void printMessage(){
+        if (model != null){
+            System.out.println("Your model is " + model);
+        }
+    }
+
 }
